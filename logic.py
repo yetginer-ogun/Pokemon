9354e540158bf7ff8f16c14732145e3ce815ebeb
yetginer-ogun <yetginer.mehmetogun@gmail.com>
2024-12-22 16:50:50 +0300

    a

--- logic.py
+++ logic.py
@@ -1,9 +1,8 @@
-import aiohttp  # Eşzamansız HTTP istekleri için bir kütüphane
+import aiohttp  
 import random
 
 class Pokemon:
     pokemons = {}
-    # Nesne başlatma (kurucu)
     def __init__(self, pokemon_trainer):
         self.pokemon_trainer = pokemon_trainer
         self.pokemon_number = random.randint(1, 1000)
@@ -14,29 +13,26 @@ class Pokemon:
             self = Pokemon.pokemons[pokemon_trainer]
 
     async def get_name(self):
-        # PokeAPI aracılığıyla bir pokémonun adını almak için eşzamansız bir yöntem
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # İstek için URL API'si
-        async with aiohttp.ClientSession() as session:  # Bir HTTP oturumu açma
-            async with session.get(url) as response:  # GET isteği gönderme
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  
+        async with aiohttp.ClientSession() as session:  
+            async with session.get(url) as response:  
                 if response.status == 200:
-                    data = await response.json()  # JSON yanıtının alınması ve kodunun çözülmesi
-                    return data['forms'][0]['name']  # Bir Pokémon'un adını döndürme
+                    data = await response.json()  
+                    return data['forms'][0]['name']  
                 else:
-                    return "Pikachu"  # İstek başarısız olursa varsayılan adı döndürür
+                    return "Pikachu"  
 
     async def info(self):
-        # Pokémon hakkında bilgi döndüren bir metot
         if not self.name:
-            self.name = await self.get_name()  # Henüz yüklenmemişse bir adın geri alınması
-        return f"Pokémonunuzun ismi: {self.name}"  # Pokémon'un adını içeren dizeyi döndürür
+            self.name = await self.get_name()  
+        return f"Pokémonunuzun ismi: {self.name}"  
 
     async def show_img(self):
-        # PokeAPI aracılığıyla bir pokémon görüntüsünün URL'sini almak için eşzamansız bir yöntem
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # İstek için URL API'si
-        async with aiohttp.ClientSession() as session:  # Bir HTTP oturumu açma
-            async with session.get(url) as response:  # GET isteği gönderme
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  
+        async with aiohttp.ClientSession() as session:  
+            async with session.get(url) as response:  
                 if response.status == 200:
-                    data = await response.json()  # JSON yanıtının alınması ve kodunun çözülmesi
-                    return data['sprites']["other"]["home"]["front_default"]  # Bir Pokémon'un adını döndürme
+                    data = await response.json()  
+                    return data['sprites']["other"]["home"]["front_default"]  
                 else:
                     return None
--- main.py
+++ main.py
@@ -3,36 +3,34 @@ from discord.ext import commands
 from config import token
 from logic import Pokemon
 
-# Bot için niyetleri (intents) ayarlama
-intents = discord.Intents.default()  # Varsayılan ayarların alınması
-intents.messages = True              # Botun mesajları işlemesine izin verme
-intents.message_content = True       # Botun mesaj içeriğini okumasına izin verme
-intents.guilds = True                # Botun sunucularla (loncalar) çalışmasına izin verme
+intents = discord.Intents.default()  
+intents.messages = True              
+intents.message_content = True       
+intents.guilds = True                
 
-# Tanımlanmış bir komut önekine ve etkinleştirilmiş amaçlara sahip bir bot oluşturma
+
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Bot çalışmaya hazır olduğunda tetiklenen bir olay
+
 @bot.event
 async def on_ready():
-    print(f'Giriş yapıldı:  {bot.user.name}')  # Botun adını konsola çıktı olarak verir
+    print(f'Giriş yapıldı:  {bot.user.name}')  
 
 # '!go' komutu
 @bot.command()
 async def go(ctx):
-    author = ctx.author.name  # Mesaj yazarının adını alma
-    # Kullanıcının zaten bir Pokémon'u olup olmadığını kontrol edin. Eğer yoksa, o zaman...
+    author = ctx.author.name  
     if author not in Pokemon.pokemons.keys():
-        pokemon = Pokemon(author)  # Yeni bir Pokémon oluşturma
-        await ctx.send(await pokemon.info())  # Pokémon hakkında bilgi gönderilmesi
-        image_url = await pokemon.show_img()  # Pokémon resminin URL'sini alma
+        pokemon = Pokemon(author)  
+        await ctx.send(await pokemon.info())  
+        image_url = await pokemon.show_img() 
         if image_url:
-            embed = discord.Embed()  # Gömülü mesajı oluşturma
-            embed.set_image(url=image_url)  # Pokémon'un görüntüsünün ayarlanması
-            await ctx.send(embed=embed)  # Görüntü içeren gömülü bir mesaj gönderme
+            embed = discord.Embed()  
+            embed.set_image(url=image_url)  
+            await ctx.send(embed=embed)  
         else:
             await ctx.send("Pokémonun görüntüsü yüklenemedi!")
     else:
-        await ctx.send("Zaten kendi Pokémonunuzu oluşturdunuz!")  # Bir Pokémon'un daha önce yaratılıp yaratılmadığını gösteren bir mesaj
-# Botun çalıştırılması
+        await ctx.send("Zaten kendi Pokémonunuzu oluşturdunuz!")  
+
 bot.run(token)
